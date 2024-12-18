6492ef32dcb0af730babf9fc4a2b72dba7196d08
ShahdEid2003 <shahdeid012@gmail.com>
2024-12-18 19:19:40 +0200

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,29 +3,42 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg sticky-top ">
-    <div className="container">
-       
-            <a className="navbar-brand logo" href="#">START BOOTSTRAP</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon" />
-            </button>
-       
-        <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">PORTFOLIO</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">ABOUT</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">CONTACT</a>
-                </li>
-                
-            </ul>
+    <nav className="navbar navbar-expand-lg navbar-light  sticky-top">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          START BOOTSTRAP
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                PORTFOLIO
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                ABOUT
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                CONTACT
+              </a>
+            </li>
+          </ul>
         </div>
-    </div>
+      </div>
     </nav>
 
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,29 +4,29 @@ export default function Footer() {
   return (
     <>
     <section className="footer">
-        <div className="container text-center">
-            <div className="row">
-                <div className="col-4">
+        <div className="container text-center ">
+            <div className="row d-flex  justify-content-center align-items-center">
+                <div className=" col-sm-4 col-6">
                     <h3>Location</h3>
                     <p>2215 John Daniel Drive
                     Clark, MO 65243</p>
                 </div>
-                <div className="col-4">
+                <div className="col-sm-4 col-6">
                  <h3>Around the Web</h3>
-                 <ul className="list-inline">
-                    <div className="d-flex gx-5 justify-content-center">
+                 <ul className="list-inline p-2">
+                    <div className="d-flex g-5 justify-content-center">
                      <li className="list-inline-item">
-                         <a className="btn btn-light btn-social mx-1" href="#">
+                         <a className="social-item " href="#">
                              <i className="fab fa-twitter"></i>
                          </a>
                      </li>
                      <li className="list-inline-item">
-                         <a className="btn btn-light btn-social mx-1" href="#">
+                         <a className="social-item " href="#">
                              <i className="fab fa-facebook-f"></i>
                          </a>
                      </li>
                      <li className="list-inline-item">
-                         <a className="btn btn-light btn-social mx-1" href="#">
+                         <a className="social-item " href="#">
                              <i className="fab fa-linkedin-in"></i>
                          </a>
                      </li>
@@ -34,7 +34,7 @@ export default function Footer() {
                  </ul>
                     
                 </div>
-                <div className="col-4">
+                <div className="col-sm-4 col-6">
                     <h3>About Freelancer</h3>
                     <p>Freelance is a free to use, MIT licensed Bootstrap theme created by Start Bootstrap .</p>
                     
